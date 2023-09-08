99dcbfbd49839530b4ec6fd6ea00992b3dea9f48
Mark M <72152638+markwmc@users.noreply.github.com>
2023-09-08 10:16:32 -0700

    Add files via upload

---
+++ RentalHouse.py
@@ -0,0 +1,29 @@
+print("Welcome back Mark")
+print(input("What is the name of this house?"))
+price = input('What is the price of this house? ')
+downpayment = float(price) * float(.2)
+downpayment = str(downpayment)
+month_mort =
+month_rent =
+rule1 =
+rule2 =
+rule3 =
+mgmtfee = month_rent*.1
+def rule2():
+
+def rule50():
+
+def rule
+
+
+'''
+print("downpayment is "+ downpayment)
+rent = input('What is the monthly rental charge? ')
+mgmt_fee = input('what is the monthly management fee? ')
+mort = input("What is the mortgage?")
+if mort + mgmt_fee < rent:
+    print("this is a bad idea")
+    print(input("Would you like to continue?\npress 'c' for continue or 'q' to quit"))
+else:
+    print('Check one passed')
+'''
